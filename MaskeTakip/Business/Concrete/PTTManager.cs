0af2219559c98b3e18b6597e345d56658530027f
Berke07 <yasuoberke@hotmail.com>
2024-02-10 20:56:33 +0300

    Mask Tracking System
    Mask Tracking System developed with C# Console App for pandemic times.

---
+++ MaskeTakip/Business/Concrete/PTTManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PTTManager:ISupplierService
+    {
+        private IApplicantService _applicantService;
+
+        public PTTManager(IApplicantService applicantService) 
+        {
+            _applicantService = applicantService;
+        }
+        public void GiveMask(Citizen citizen)
+        {
+            
+            if (_applicantService.ChechkCitizen(citizen)) 
+            {
+                Console.WriteLine(citizen.Name + "için maske verildi.");
+            }
+            else
+            {
+                Console.WriteLine(citizen.Name + "için maske verilemedi.");
+            }
+        }
+    }
+}
---
+++ MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,68 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+
+public class Program
+    {
+        private static void Main(string[] args)
+        {
+
+            SayHi(name:"Berke");
+            SayHi();
+            int result = Addition(50);
+
+            string stuedent1 = "Berke";
+            string stuedent2 = "Dogukan";
+            string stuedent3 = "Ahmet";
+
+            string[] stuedents = new string[3];
+            stuedents[0] = "Berke";
+            stuedents[1] = "Dogukan";
+            stuedents[2] = "Ahmet";
+
+            stuedents = new string[4];
+
+            for (int i = 0; i < stuedents.Length; i++)
+            {
+                Console.WriteLine(stuedents[i]);
+            }
+
+            string[] cities1 = new[] { "Ankara", "İstanbul", "İzmir" };
+            string[] cities2 = new[] { "Bursa", "Antalya", "Diyarbakır" };
+
+            foreach (string city in cities1)
+            {
+                Console.WriteLine(city);
+            }
+
+            List<string> newCities1 = new List<string>{ "Ankara 1", "İstanbul 1", "İzmir 1" };
+            newCities1.Add(item: "Bartın 1");
+            foreach (var city in newCities1)
+            {
+                Console.WriteLine(city);
+            }
+
+
+        Citizen citizen1 = new Citizen();
+        citizen1.Name = "Hamza Berke Can";
+        citizen1.Surname = "Kösali";
+        citizen1.NationalIdentity = 12345678910;
+        citizen1.BornYear = 2002;
+
+        PTTManager pttManager = new PTTManager(new CitizenManager());
+        pttManager.GiveMask(citizen1);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name="Noname")
+        {
+            Console.WriteLine("Hello " + name);
+        }
+
+        static int Addition(int sayi1 = 7, int sayi2 = 9) 
+        {
+            int result = sayi2 + sayi1;
+            Console.WriteLine("Toplam " + result.ToString());
+            return result; 
+        }
+    }
